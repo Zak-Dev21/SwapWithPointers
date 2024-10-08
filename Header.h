f9c46647762864f4967bb893e89d5b35ba89fb61
Zak <zakariabouaddou@gmail.com>
2024-10-08 16:43:20 -0700

    adding detailed comments and adjusted code to properly include a header.

--- Header.h
+++ Header.h
@@ -1,10 +1,10 @@
-#ifndef Header.h
-#define Header.h
+#ifndef Header_h
+#define Header_h
 
-void swap(int* x, int* y) {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+void swap(int* x, int* y) { // defining fct that takes two integer addresses
+    int temp = *x; //creating temporary variable to store memory address for x
+    *x = *y; // assign memory address of x to memory address of y
+    *y = temp; // assign memory address of y to temporary variable (point to address of x)
 }
 
 #endif
--- SwapWithPointers.cpp
+++ SwapWithPointers.cpp
@@ -1,12 +1,7 @@
 
 #include <iostream>
+#include "Header.h"
 using namespace std;
-
-void swap(int* x, int* y) {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
 
 int main()
 {
