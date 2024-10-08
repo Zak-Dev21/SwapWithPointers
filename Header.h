d878f228e8f81ea1f52a99ac25ed4d6b0785ed98
Zak <zakariabouaddou@gmail.com>
2024-10-08 16:47:57 -0700

    ajusting to clear comment for fct definiton.

--- Header.h
+++ Header.h
@@ -1,7 +1,8 @@
 #ifndef Header_h
 #define Header_h
 
-void swap(int* x, int* y) { // defining fct that takes two integer addresses
+// defining fct that swaps the value of two integers using their addresses
+void swap(int* x, int* y) {
     int temp = *x; //creating temporary variable to store memory address for x
     *x = *y; // assign memory address of x to memory address of y
     *y = temp; // assign memory address of y to temporary variable (point to address of x)
